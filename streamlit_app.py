bb26cf8584845333b537b2ef8a5c159c1e4207be
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2021-06-07 00:59:44 +0100

    new changes for presentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,118 @@ from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities.engine import OperatorConfig
 from presidio_evaluator.data_generator.presidio_perturb import PresidioPerturb
-
 from presidio_image_redactor import ImageRedactorEngine
 from PIL import Image
 import pandas as pd
+import spacy
 
+def show_pipeline(analyzer_results, perturbed_text, original_text: str):
+    TEXT_LINE = """
+            <head>
+            <style>
+            body {{
+                font-family: 'Poiret One';font-size: 22px;
+                }}
+            h3 {{
+            text-align: center;
+            }}
+            .center {{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            height: 50%;
+            }}
+            .loading {{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            }}
+            </style>
+            </head>
+            <body>
+            <img src="https://s6.gifyu.com/images/input-onlinegiftools.gif" width="25" class="loading">
+            <h3>{}</h3 >           
+            <img src="https://media.giphy.com/media/ZbfHMCu2BCuEdZCjBC/giphy.gif" width="155" class="center">
+            <img src="https://s6.gifyu.com/images/input-onlinegiftools.gif" width="25" class="loading">
+            </body >
+            """
+    
 
-def anon_text(text):
+    st.markdown(TEXT_LINE.format("Tokenizer"), unsafe_allow_html=True)
+
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(original_text)
+    
+    with st.beta_expander("Check the analyzed Token:"):
+        for token in doc:
+            temp_str = """
+                <head>
+                <style>
+                p {{
+                text-align: center;
+                }}
+                </style>
+                </head>
+                <body>
+                <p>{}</p>
+                <p>Token: {}</p>
+                <p>Lemma: {}</p>
+                <p>Pos: {}</p>
+                </body >
+                """.format("_"*50,token.text, token.lemma_, token.pos_)
+            st.markdown(temp_str, unsafe_allow_html=True)
+    
+    st.markdown(TEXT_LINE.format("Analyzer Engine and NER"), unsafe_allow_html=True)
+
+    with st.beta_expander("Check Analyzed Result"):
+        for entity in analyzer_results:
+            if entity.score > 0.05:
+                temp_str = """
+                    <head>
+                    <style>
+                    p {{
+                    text-align: center;
+                    }}
+                    </style>
+                    </head>
+                    <body>
+                    <p>{}</p>
+                    <p>     Entity Type: {}</p> 
+                    <p>     Start: {}</p>
+                    <p>     End: {}</p>
+                    <p>     Score: {}</p>
+                    </body >
+                    """.format("_"*50, entity.entity_type, entity.start, entity.end, entity.score)
+                st.markdown(temp_str, unsafe_allow_html=True)
+
+    st.markdown(TEXT_LINE.format("Perturbator Engine"), unsafe_allow_html=True)
+
+    with st.beta_expander("Check Perturbator Result"):
+        temp_str = """
+                    <head>
+                    <style>
+                    p {{
+                    text-align: center;
+                    }}
+                    </style>
+                    </head>
+                    <body>
+                    <p>{}</p>
+                    </body >
+                    """.format(perturbed_text)
+        st.markdown(temp_str ,unsafe_allow_html=True)
+    return
+
+def anon_text(text, pipeline = False):
 
     analyzer = AnalyzerEngine()
-
     anonymizer = AnonymizerEngine()
+
     fake_pii_df = pd.read_csv(
         'raw_data/FakeNameGenerator.com_3000.csv', encoding='utf-8')
     presidio_perturb = PresidioPerturb(fake_pii_df=fake_pii_df)
 
-
     analyzer_results = analyzer.analyze(text = text, language ='en')
-    
     anonymized_results = anonymizer.anonymize(
         text = text,
         analyzer_results = analyzer_results,
@@ -35,30 +129,33 @@ def anon_text(text):
             "NRP": OperatorConfig("replace", {"new_value": "-MISC_DATA-"})
         }
     )
-    print("_"*100, anonymized_results.text)
+
     text_perturbed = presidio_perturb.perturb(original_text=text, presidio_response=analyzer_results, count=1)
-    st.markdown(
-        """
-        <p><h3 style="color:LightCoral;">Analysis Result:</h3></p>
+    if pipeline:
+        show_pipeline(analyzer_results, text_perturbed[0], text)
+    else:
+        st.markdown(
+            """
+            <p><h3 style="color:LightCoral;">Analysis Result:</h3></p>
 
-        ### {}
-        
-        <p><h3 style="color:LightCoral;">Perturbed:</h3></p>
+            ### {}
+            
+            <p><h3 style="color:LightCoral;">Perturbed:</h3></p>
 
-        ### {}
-        ###
-        ### Check entities detected:
-        """.format(str(anonymized_results.text), text_perturbed[0]), 
-        unsafe_allow_html=True)
+            ### {}
+            ###
+            ### Check entities detected:
+            """.format(str(anonymized_results.text), text_perturbed[0]), 
+            unsafe_allow_html=True)
 
-    for entity in analyzer_results:
-        if entity.score > 0.05:
-            with st.beta_expander("Entity type: {}".format(entity.entity_type)):
-                st.markdown(
-                    """
-                    Start: {} | End: {} | Score: {}
-                    """.format(entity.start, entity.end, entity.score)
-                    )
+        for entity in analyzer_results:
+            if entity.score > 0.05:
+                with st.beta_expander("Entity type: {}".format(entity.entity_type)):
+                    st.markdown(
+                        """
+                        Start: {} | End: {} | Score: {}
+                        """.format(entity.start, entity.end, entity.score)
+                        )
 
 def anon_image(image):
     image = Image.open(image)
@@ -98,44 +195,106 @@ st.sidebar.markdown(
     <a href="https://twitter.com/LorenzoDemiri"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" /></a>
     
     """, 
-    unsafe_allow_html=True
-)
-
-st.title("D.ANON")
+    unsafe_allow_html=True)
 
 st.markdown("""
-    D.ANON (Data Anonymization Tool) is a software that finds and hides all the sensitive data
+                    <head>
+                    <link href='https://fonts.googleapis.com/css?family=Poiret One' rel='stylesheet'>
+                    <style>
+                    <style>
+                    body {
+                        font-family: 'Poiret One';
+                    }
+                    h1 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    font-size: 70px;
+                    }
+                    h2 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    }
+                    h3 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    }
+                    h4 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    }
+                    h5 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    } 
+                    h6 {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    }
+                    p {
+                    font-family: 'Poiret One';
+                    text-align: center;
+                    font-weight: bold;
+                    }                                                       
+                    .center {
+                    display: block;
+                    margin-left: auto;
+                    margin-right: auto;
+                    height: 50%;
+                    }
+                    .loading {
+                    display: block;
+                    margin-left: auto;
+                    margin-right: auto;
+                    }
+                    </style>
+                    </head>
+                    <body>
+                    <h1>S E C R E T U M</p> 
+                    <p>(    Data Anonymization Tool)</p>
+                    <img src="https://media.giphy.com/media/lhwgPv0AuJehQ2S8cb/giphy.gif" width="300" class="loading">
+                    </body >
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    SECRETUM (Data Anonymization Tool) is a software that finds and hides all the sensitive data
     such as: Name, Email, Telephone Number and etc. from a text.
 
     The data inserted on the app are not stored or memorized, 
     data privacy first priority ;)
 """)
-st.image("strem_img/dataprotection_shutterstock_382458778.jpg")
 
 st.markdown("""
 Try our tool, insert a text or an image containing sensitive information then press the button Anonymize Data.
 \n(Ex. Hi I'm Mark Spencer and I live in Whashington)
 """)
-
-text = st.text_area("Insert text to Anonymize")
+file_upload = st.file_uploader(
+    "Upload an image containing a documente to Anonymize or with your smartphone snap a photo to document.")
 st.markdown("""
-## or
-## """)
-file_upload = st.file_uploader("Upload an image containing a documente to Anonymize or with your smartphone snap a photo to document.")
-
+                <head>
+                <style>
+                p {{
+                text-align: center;
+                }}
+                </style>
+                </head>
+                <body>
+                <p>Or</p>
+                </body >
+                """, unsafe_allow_html=True)
+text = st.text_area("Insert text to Anonymize")
+show_pl = st.checkbox("Show Pipeline")
 button = st.button("Anonymize Data")
 
-
 if button  and text != "":
-    anon_text(text)
+    anon_text(text, show_pl)
 elif button and file_upload is not None:
     anon_image(file_upload)
 
-st.markdown("More info")
+st.markdown("More info about this project")
 with st.beta_expander("Why do we need Data Anonymization?"):
     st.markdown("""
     ### "Data is the new oil" 
-    \nThe concept behind this project. 
+    \nIs concept behind this project. 
     Today we live in society where the Data are a new technology assets for companies and goverments, by analyzing and processing those data
     we can build new technologies and products that can improve our lives drastically. 
     However, such as all the new disruptive technologies, there's always a negative side,
